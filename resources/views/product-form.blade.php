7399a55c8cb68a5f615b1069a1a907d0db1ea55c
Muhammad Naufal <muhammadnaufal02881@gmail.com>
2023-11-22 12:30:50 +0700

    update

--- resources/views/product-form.blade.php
+++ resources/views/product-form.blade.php
@@ -1,78 +1,49 @@
-<!-- resources/views/product-form.blade.php -->
+@extends('utama')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product Information</title>
-    <style>
-        body {
-            background-color: #e0eaf0;
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
+@section('content')
+<form method="POST" action="/process-product">
+    @csrf
+    <div class="col-md-6 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Default form</h4>
+            <p class="card-description">
+              Basic form layout
+            </p>
+            <form class="forms-sample">
+              <div class="form-group">
+                <label for="exampleInputUsername1">Kode Barang</label>
+                <input type="text" class="form-control" id="exampleInputUsername1" placeholder="text" name="KodeBarang">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Nama Barang</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="text" name="NamaBarang">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Jenis Varian</label>
+                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="text" name="JenisVarian">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputConfirmPassword1">Qty</label>
+                <input type="number" class="form-control" id="exampleInputConfirmPassword1" placeholder="number" name="Qty">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputConfirmPassword1">Harga Jual</label>
+                <input type="number" class="form-control" id="exampleInputConfirmPassword1" placeholder="number" name="HargaJual">
+              </div>
+              <div class="form-check form-check-flat form-check-primary">
+                <label class="form-check-label">
+                  <input type="checkbox" class="form-check-input">
+                  Remember me
+                </label>
+              </div>
+              <button type="submit" class="btn btn-primary me-2">Submit</button>
+              <button class="btn btn-light">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </div>
+</form>
 
-        form {
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 400px; /* Adjust the width as needed */
-        }
-
-        label {
-            display: block;
-            margin-bottom: 8px;
-            color: #1565c0; /* Blue color for labels */
-        }
-
-        input {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 16px;
-            box-sizing: border-box;
-            border: 1px solid #1565c0; /* Blue color for borders */
-            border-radius: 4px;
-        }
-
-        input[type="submit"] {
-            background-color: #1565c0; /* Blue color for the submit button */
-            color: #ffffff;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #003c8f; /* Darker blue color on hover */
-        }
-    </style>
-</head>
-<body>
-    <form method="POST" action="{{ route('process-product') }}">
-        
-        @csrf
-        <label for="KodeBarang">Kode Barang:</label>
-        <input type="text" name="KodeBarang" required>
-        <br>
-        <label for="NamaBarang">Nama Barang:</label>
-        <input type="text" name="NamaBarang" required>
-        <br>
-        <label for="JenisVarian">Jenis Varian:</label>
-        <input type="text" name="JenisVarian" required>
-        <br>
-        <label for="Qty">Qty:</label>
-        <input type="number" name="Qty" required>
-        <br>
-        <label for="HargaJual">Harga Jual:</label>
-        <input type="number" name="HargaJual" required>
-        <br>
-        <input type="submit" value="Proses">
-    </form>
-</body>
-</html>
+@endsection
+ 
